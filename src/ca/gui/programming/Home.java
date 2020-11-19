761d35bdc20324f00e7b59a2168c9575dabaf19b
alychinque <alychinque@gmail.com>
2020-11-19 10:09:28 +0000

    Header was created.

--- src/ca/gui/programming/Home.java
+++ src/ca/gui/programming/Home.java
@@ -1,6 +1,10 @@
 package ca.gui.programming;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
@@ -20,6 +24,20 @@ public class Home  extends JFrame {
         this.setResizable(false);
         this.setVisible(true);
         this.setLocationRelativeTo(null);
+        
+       //CREATED A PANEL AND A LABEL
+        JPanel top = new JPanel();
+        JLabel style = new JLabel("STYLE BARBER SHOP");
+
+        //SET HEADER
+        top.setBackground(new java.awt.Color(0, 0, 0));
+        style.setFont(new Font("Showcard Gothic", Font.PLAIN, 48));
+        style.setForeground(new java.awt.Color(255, 204, 0));
+        //ADDED LABEL TO PANEL AND PANEL TO FRAME
+        top.add(style);
+        this.add(top, BorderLayout.PAGE_START);
+
+        
     }
     
 }
