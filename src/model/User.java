9e3a42adc2799bf9b1eb4c6f4806fede62e848f0
alychinque <alychinque@gmail.com>
2020-11-21 03:03:21 +0000

    Added getters and setters to User

--- src/model/User.java
+++ src/model/User.java
@@ -23,6 +23,46 @@ public class User {
         this.email = email;
         this.password = password;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
     
 }
