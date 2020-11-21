8cbfd36b3d33c501358e3419178ec0b97a70a1b2
alychinque <alychinque@gmail.com>
2020-11-21 03:01:28 +0000

    Created User and give to it attributes.

---
+++ src/model/User.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author alych
+ */
+public class User {
+    private String firstName;
+    private String surname;
+    private int phone;
+    private String email;
+    private String password;
+
+   
+    
+    
+}
