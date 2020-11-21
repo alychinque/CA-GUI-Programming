d2107bbd099b5c0c8d844b1b51a5d8bc234a9ade
alychinque <alychinque@gmail.com>
2020-11-21 03:10:19 +0000

    Methods getters and setters were added to Barber class.

--- src/model/Barber.java
+++ src/model/Barber.java
@@ -29,6 +29,71 @@ public class Barber {
         this.email = email;
         this.password = password;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getBarberShop() {
+        return barberShop;
+    }
+
+    public void setBarberShop(String barberShop) {
+        this.barberShop = barberShop;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     
     
 }
