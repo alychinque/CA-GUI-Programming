c48a707a701ca9fe4411daf633c7ce054f6304f8
alychinque <alychinque@gmail.com>
2020-11-21 03:07:29 +0000

    Barber class was created and attributes were added to it.

---
+++ src/model/Barber.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author alych
+ */
+public class Barber {
+    private String firstName;
+    private String surname;
+    private String barberShop;
+    private int phone;
+    private String address;
+    private String location;
+    private String email;
+    private String password;
+    
+}
