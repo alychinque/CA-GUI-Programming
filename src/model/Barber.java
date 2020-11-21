385c390c2e0d2a76ffe1dade20c6394678d83573
alychinque <alychinque@gmail.com>
2020-11-21 03:09:05 +0000

    Constructor was created in the class Barber.

--- src/model/Barber.java
+++ src/model/Barber.java
@@ -18,5 +18,17 @@ public class Barber {
     private String location;
     private String email;
     private String password;
+
+    public Barber(String firstName, String surname, String barberShop, int phone, String address, String location, String email, String password) {
+        this.firstName = firstName;
+        this.surname = surname;
+        this.barberShop = barberShop;
+        this.phone = phone;
+        this.address = address;
+        this.location = location;
+        this.email = email;
+        this.password = password;
+    }
+    
     
 }
