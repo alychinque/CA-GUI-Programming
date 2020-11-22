06c64cbe10ac65281a0601e7241e4c48a31bc881
alychinque <alychinque@gmail.com>
2020-11-22 01:04:00 +0000

    View Login class was created.

---
+++ src/view/Login.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+/**
+ *
+ * @author alych
+ */
+public class Login extends JFrame {
+
+    public Login() {
+        this.setTitle("Style Barber Shop - login");
+        this.setSize(900, 600);
+        this.setResizable(false);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        BorderLayout frameLayout = new BorderLayout();
+        this.setLayout(frameLayout);
+
+        //CREATED A PANEL AND A LABEL
+        JPanel top = new JPanel();
+        JLabel style = new JLabel("STYLE BARBER SHOP");
+
+        //SET HEADER
+        top.setBackground(Color.black);
+        top.setBorder(BorderFactory.createLineBorder(Color.orange));
+        style.setFont(new Font("Showcard Gothic", Font.PLAIN, 48));
+        style.setForeground(new java.awt.Color(255, 204, 0));
+    }
+
+}
