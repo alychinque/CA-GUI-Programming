48557acc4d3945f5ec37478ae010fe4e826e1fff
alychinque <alychinque@gmail.com>
2020-11-22 23:46:19 +0000

    Added labels and text fields to Customer login.

--- src/view/Login.java
+++ src/view/Login.java
@@ -9,10 +9,12 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 /**
@@ -66,6 +68,33 @@ public class Login extends JFrame {
         left.setBackground(Color.black);
         left.setBorder(BorderFactory.createLineBorder(Color.orange));
         left.setBounds(0, 110, 448, 434); 
+        
+        //CUSTOMER LOGIN
+        JLabel customer = new JLabel("CUSTOMER");
+        customer.setFont(new Font("Showcard Gothic", Font.PLAIN, 28));
+        customer.setForeground(new java.awt.Color(255, 204, 0));
+        customer.setBounds(150, 90, 250, 40);
+        left.add(customer);
+        
+        JLabel emailC = new JLabel("EMAIL:");
+        emailC.setBounds(120, 150, 200, 30);
+        left.add(emailC);
+        
+        JTextField emailCustomer = new JTextField(10);
+        emailCustomer.setBounds(120, 180, 200, 30);
+        left.add(emailCustomer);
+        
+        JLabel passC = new JLabel("PASSWORD:");
+        passC.setBounds(120, 240, 200, 30);
+        left.add(passC);
+        
+        JTextField passCustomer = new JTextField(10);
+        passCustomer.setBounds(120, 270, 200, 30);
+        left.add(passCustomer);
+        
+        JButton loginC = new JButton("LOG IN");
+        loginC.setBounds(170, 350, 100, 20);
+        left.add(loginC);
         
         this.add(left);
         
