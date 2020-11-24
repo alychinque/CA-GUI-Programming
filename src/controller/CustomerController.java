2ba5f9d94365b3d465106e116ddbe363e9df5c41
alychinque <alychinque@gmail.com>
2020-11-24 23:07:01 +0000

    Created customer Controller.

---
+++ src/controller/CustomerController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import view.Customer;
+
+/**
+ *
+ * @author Alysson Chinque
+ */
+public class CustomerController implements ActionListener{
+    private final Customer view;
+
+    public CustomerController(Customer view) {
+        this.view = view;
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
