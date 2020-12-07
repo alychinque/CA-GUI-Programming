f699dde964bc83fa651dc0a39b153d0f9dd5b4e5
alychinque <alychinque@gmail.com>
2020-12-07 14:42:40 +0000

    Updated RegController class and added registerBarber case.

--- src/controller/RegistrationController.java
+++ src/controller/RegistrationController.java
@@ -10,8 +10,10 @@ import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
+import model.Barber;
 import model.DAO.ConnectionDB;
 import model.DAO.UserDAO;
+import model.DAO.barberDAO;
 import model.User;
 import view.Home;
 import view.Registration;
@@ -20,9 +22,18 @@ import view.Registration;
  *
  * @author Alysson Chinque
  */
-public class RegistrationController implements ActionListener{
+public class RegistrationController implements ActionListener {
+
     private final Registration view;
-    
+    private String name;
+    private String surname;
+    private int phone;
+    private String email;
+    private String pass;
+    private String barberShop;
+    private String address;
+    private String location;
+
     public RegistrationController(Registration view) {
         this.view = view;
     }
@@ -36,38 +47,66 @@ public class RegistrationController implements ActionListener{
                 this.view.dispose();
                 home.setVisible(true);
                 break;
-                
+
             case "customer":
                 this.view.setBarberSection(false);
                 this.view.setRegisterCust(true);
                 break;
-                
+
             case "barber":
                 this.view.setBarberSection(true);
                 this.view.setRegisterCust(false);
                 break;
-                
+
             case "registerCust":
                 //receiving custumers' attributes
-                String name = view.getFirstName().getText();
-                String surname = view.getSurname().getText();
-                int phone = Integer.parseInt(view.getPhone().getText());
-                String email = view.getEmail().getText();
-                String pass = view.getPassword().getText();
+                name = view.getFirstName().getText();
+                surname = view.getSurname().getText();
+                phone = Integer.parseInt(view.getPhone().getText());
+                email = view.getEmail().getText();
+                pass = view.getPassword().getText();
                 //creating a new user 
                 User user = new User(name, surname, phone, email, pass);
-            try {
-                //creating a new Connection conn and giving connection with the DB
-                Connection conn = new ConnectionDB().getConnection();
+                try {
+                    //creating a new Connection conn and giving connection with the DB
+                    Connection conn = new ConnectionDB().getConnection();
+
+                    //instantiating userDAO passing a connection as a parameter
+                    UserDAO userDAO = new UserDAO(conn);
+
+                    //accessing method insert in the userDAO class passing a user as a parameter
+                    userDAO.insert(user);
+                } catch (SQLException ex) {
+                    java.util.logging.Logger.getLogger(RegistrationController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                break;
+
+            case "registerBarber":
+                //receiving barbers' attributes
+                name = view.getFirstName().getText();
+                surname = view.getSurname().getText();
+                barberShop = view.getBarberShop().getText();
+                phone = Integer.parseInt(view.getPhone().getText());
+                address = view.getAddress().getText();
+                location = view.getCombo();
+                email = view.getEmail().getText();
+                pass = view.getPassword().getText();
                 
-                //instantiating userDAO passing a connection as a parameter
-                UserDAO userDAO = new UserDAO(conn);
-                
-                //accessing method insert in the userDAO class passing a user as a parameter
-                userDAO.insert(user);
-            } catch (SQLException ex) {
-                java.util.logging.Logger.getLogger(RegistrationController.class.getName()).log(Level.SEVERE, null, ex);
-            }
+                //creating a new user 
+                Barber barber = new Barber(name, surname, barberShop, phone, address, location, email, pass);
+                try {
+                    //creating a new Connection conn and giving connection with the DB
+                    Connection conn = new ConnectionDB().getConnection();
+
+                    //instantiating userDAO passing a connection as a parameter
+                    barberDAO barberDAO = new barberDAO(conn);
+
+                    //accessing method insert in the userDAO class passing a user as a parameter
+                    barberDAO.insert(barber);
+                } catch (SQLException ex) {
+                    java.util.logging.Logger.getLogger(RegistrationController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                break;
         }
-    } 
+    }
 }
