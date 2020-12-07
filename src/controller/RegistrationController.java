13648965c1f61c5ef161a73a37d55e392b69e3fd
alychinque <alychinque@gmail.com>
2020-12-07 16:46:50 +0000

    validateName and isValidName methods were created in RegistrationController class.

--- src/controller/RegistrationController.java
+++ src/controller/RegistrationController.java
@@ -10,6 +10,8 @@ import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
+import java.util.regex.Pattern;
+import javax.swing.JOptionPane;
 import model.Barber;
 import model.DAO.ConnectionDB;
 import model.DAO.UserDAO;
@@ -25,6 +27,7 @@ import view.Registration;
 public class RegistrationController implements ActionListener {
 
     private final Registration view;
+    private String[] fullName;
     private String name;
     private String surname;
     private int phone;
@@ -41,31 +44,34 @@ public class RegistrationController implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
+            //instatiating Home and turn visible true while turn Registration off
             case "back":
-                //instatiating Home and turn visible true while turn Registration off
                 Home home = new Home();
                 this.view.dispose();
                 home.setVisible(true);
                 break;
-
+            //show customer section and hide barber section
             case "customer":
                 this.view.setBarberSection(false);
                 this.view.setRegisterCust(true);
                 break;
-
+            //show barber section and hide customer section
             case "barber":
                 this.view.setBarberSection(true);
                 this.view.setRegisterCust(false);
                 break;
 
             case "registerCust":
-                //receiving custumers' attributes
-                name = view.getFirstName().getText();
-                surname = view.getSurname().getText();
-                phone = Integer.parseInt(view.getPhone().getText());
-                email = view.getEmail().getText();
-                pass = view.getPassword().getText();
-                //creating a new user 
+                //validate a user
+                if(validateName()){
+                    JOptionPane.showMessageDialog(null, "Success");
+                }else{
+                    JOptionPane.showMessageDialog(null, "Registration failed");
+                }
+//                validatePhone();
+//                validateEmail();
+                //validatedPass();
+                //creating a new user receiving a validated user
                 User user = new User(name, surname, phone, email, pass);
                 try {
                     //creating a new Connection conn and giving connection with the DB
@@ -82,17 +88,19 @@ public class RegistrationController implements ActionListener {
                 break;
 
             case "registerBarber":
-                //receiving barbers' attributes
-                name = view.getFirstName().getText();
-                surname = view.getSurname().getText();
+                if(validateName()){
+                    JOptionPane.showMessageDialog(null, "Success");
+                }else{
+                    JOptionPane.showMessageDialog(null, "Registration failed");
+                }
                 barberShop = view.getBarberShop().getText();
                 phone = Integer.parseInt(view.getPhone().getText());
                 address = view.getAddress().getText();
                 location = view.getCombo();
                 email = view.getEmail().getText();
                 pass = view.getPassword().getText();
-                
-                //creating a new user 
+
+                //creating a new barber 
                 Barber barber = new Barber(name, surname, barberShop, phone, address, location, email, pass);
                 try {
                     //creating a new Connection conn and giving connection with the DB
@@ -108,5 +116,49 @@ public class RegistrationController implements ActionListener {
                 }
                 break;
         }
+    }
+
+    private Boolean validateName() {
+        //receiving custumers' attributes
+        try {
+            fullName = view.getFullName().getText().trim().split(" ");
+            if (fullName.length >= 2) {
+                name = fullName[0].toLowerCase();
+                surname = fullName[fullName.length - 1].toLowerCase();
+                return isValidName(name, surname);
+            }else{
+                JOptionPane.showMessageDialog(null, "Failed!\nPlease enter name and surname with a space between them!");
+                return false;
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error");
+            return false;
+        }
+    }
+
+    private boolean isValidName(String name, String surname) {
+        boolean nameBoo = Pattern.matches("[a-z]+", name);
+        boolean surnameBoo = Pattern.matches("[a-z]+", surname);
+        if (nameBoo && surnameBoo == true) {
+
+            return true;
+        } else {
+            if (!nameBoo) {
+                JOptionPane.showMessageDialog(null, "Name is not valid!\nPlease Enter again!");
+            }if (!surnameBoo) {
+                JOptionPane.showMessageDialog(null, "Surname is not valid!\nPlease Enter again!");
+            }
+            return false;
+        }
+    }
+
+    private void validatePhone() {
+        phone = Integer.parseInt(view.getPhone().getText());
+
+    }
+
+    private void validateEmail() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
