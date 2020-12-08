81785a24433b5487b213513d4f27123c0184e10a
alychinque <alychinque@gmail.com>
2020-12-08 14:31:57 +0000

    Update UserDAO.java

--- src/model/DAO/UserDAO.java
+++ src/model/DAO/UserDAO.java
@@ -8,6 +8,7 @@ package model.DAO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import model.User;
 
 /**
@@ -24,10 +25,16 @@ public class UserDAO {
 
     public void insert(User user) throws SQLException {
         String query = "INSERT INTO customer(name_customer, surname_customer, phone_customer, email_customer, password_customer)"
-                + "VALUES ('"+user.getFirstName()+"', '"+user.getSurname()+"', "+user.getPhone()+", '"+user.getEmail()+"', '"+user.getPassword()+"')";
-
+                + "VALUES (?, ?, ?, ?, ?)";
+                //'"+user.getFirstName()+"', '"+user.getSurname()+"', "+user.getPhone()+", '"+user.getEmail()+"', '"+user.getPassword()+"'
         // Get a statement from the connection
-        PreparedStatement stmt = connection.prepareStatement(query);
+//        PreparedStatement Statement = ;
+        PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        stmt.setString(1, user.getFirstName());
+        stmt.setString(2, user.getSurname());
+        stmt.setInt(3, user.getPhone());
+        stmt.setString(4, user.getEmail());
+        stmt.setString(5, user.getPassword());
         stmt.execute();
         connection.close();
     }
