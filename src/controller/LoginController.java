3df640dcd4a0f3540ab311dd097ebb09d0f00051
alychinque <alychinque@gmail.com>
2020-12-09 04:16:09 +0000

    Created customer option in LoginController class.

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -7,6 +7,12 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import javax.swing.JOptionPane;
+import model.DAO.ConnectionDB;
+import model.DAO.UserDAO;
 import view.Customer;
 import view.Home;
 import view.Login;
@@ -26,24 +32,43 @@ public class LoginController implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        if (e.getActionCommand().equals("combo")){
-            if(this.view.getCombo().equals("CUSTOMER")){
-                this.view.changeVisibilityCustomer(true);
-                this.view.changeVisibilityBarber(false);
-            }else{
-                this.view.changeVisibilityCustomer(false);
-                this.view.changeVisibilityBarber(true);
-            }
-        }else if(e.getActionCommand().equals("back")){
-            Home home = new Home();
-            this.view.setVisible(false);
-            home.setVisible(true);
-        }else if(e.getActionCommand().equals("customer")){
-            Customer cust = new Customer();
-            cust.Customer();
-            this.view.setVisible(false);
-        }else if(e.getActionCommand().equals("barber")){
-            
+        switch (e.getActionCommand()) {
+            case "combo":
+                if(this.view.getCombo().equals("CUSTOMER")){
+                    this.view.changeVisibilityCustomer(true);
+                    this.view.changeVisibilityBarber(false);
+                }else{
+                    this.view.changeVisibilityCustomer(false);
+                    this.view.changeVisibilityBarber(true);
+                }   break;
+            case "back":
+                Home home = new Home();
+                this.view.setVisible(false);
+                home.setVisible(true);
+                break;
+            case "customer":
+                try {
+                    //creating a new Connection conn and giving connection with the DB
+                    Connection conn = new ConnectionDB().getConnection();
+
+                    //instantiating userDAO passing a connection as a parameter
+                    UserDAO userDAO = new UserDAO(conn);
+
+                    //accessing method checkCustumer in the userDAO class passing email and password as a parameter
+                    int id = userDAO.checkCustomer(this.view.getEmailCustomer().getText(), this.view.getPassCustomer().getText());
+                    Customer cust = new Customer();
+                    cust.Customer(id);
+                    this.view.setVisible(false);
+                    JOptionPane.showMessageDialog(null, "Welcome!");
+                    
+                } catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(null, "User not found!\nPlease enter a valid email and password!\n\nOr Register");
+                    java.util.logging.Logger.getLogger(RegistrationController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                break;
+            case "barber":
+                break;
         }
     }
     
