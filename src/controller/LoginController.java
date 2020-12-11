f12a1787db3b60b83d8b358e31e307f7b77be038
alychinque <alychinque@gmail.com>
2020-12-11 03:27:11 +0000

    Created Login for barbers in LoginController.

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -10,9 +10,12 @@ import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
+import model.Barber;
 import model.DAO.ConnectionDB;
 import model.DAO.UserDAO;
+import model.DAO.BarberDAO;
 import model.User;
+import view.BarberView;
 import view.Customer;
 import view.Home;
 import view.Login;
@@ -67,7 +70,7 @@ public class LoginController implements ActionListener {
                     User validUser = userDAO.checkCustomer(email, pass);
                     
                     if (validUser == null){
-                        JOptionPane.showMessageDialog(null, "User not found!\nPlease enter a valid email and password!\n\nOr Register");
+                        JOptionPane.showMessageDialog(null, "User not found!\nPlease enter a valid email and password!\nIf you are new, please back and register");
                         break;
                     }else{
                         Customer cust = new Customer();
@@ -75,11 +78,44 @@ public class LoginController implements ActionListener {
                         this.view.setVisible(false);
                     }
                 } catch (SQLException ex) {
-                    JOptionPane.showMessageDialog(null, "User not found!\nPlease enter a valid email and password!\n\nOr Register");
+                    JOptionPane.showMessageDialog(null, "Error");
+                    break;
                 }
                 
                 break;
             case "barber":
+                try {
+                    //creating a new Connection conn and giving connection with the DB
+                    Connection conn = new ConnectionDB().getConnection();
+
+                    //instantiating userDAO passing a connection as a parameter
+                    BarberDAO barberDAO = new BarberDAO(conn);
+                    
+                    //data customer
+                    String email = this.view.getEmailBarber().getText();
+                    String pass = this.view.getPassBarber().getText();
+                    
+                    if (email.equals("") || pass.equals("")){
+                        JOptionPane.showMessageDialog(null, "Please fill the fields up!");
+                        break;
+                    }
+
+                    //accessing method checkCustumer in the userDAO class passing email and password as a parameter
+                    Barber validBarber = barberDAO.checkBarber(email, pass);
+                    
+                    if (validBarber == null){
+                        JOptionPane.showMessageDialog(null, "Barber not found!\nPlease enter a valid email and password!\nIf you are new, please back and register");
+                        break;
+                    }else{
+                        BarberView bar = new BarberView();
+                        bar.BarberView(validBarber);
+                        this.view.setVisible(false);
+                    }
+                } catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(null, "Error");
+                    break;
+                }
+                
                 break;
         }
     }
