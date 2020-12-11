fe2d80be8ded81492926da7f93d3fac1295bf967
alychinque <alychinque@gmail.com>
2020-12-11 11:08:07 +0000

    Create SetSlotsController.java

---
+++ src/controller/SetSlotsController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import view.SetSlots;
+
+/**
+ *
+ * @author alych
+ */
+public class SetSlotsController implements ActionListener{
+    private final SetSlots view;
+
+    public SetSlotsController(SetSlots view) {
+        this.view = view;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
