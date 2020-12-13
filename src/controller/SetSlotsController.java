6ed335a83b7e7be275f65c031e6b1ac8a9ba07f0
alychinque <alychinque@gmail.com>
2020-12-13 05:05:58 +0000

    First try, building ADD event.

--- src/controller/SetSlotsController.java
+++ src/controller/SetSlotsController.java
@@ -5,16 +5,24 @@
  */
 package controller;
 
+import com.toedter.calendar.JDateChooser;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import view.SetSlots;
 
 /**
  *
- * @author alych
+ * @author Alysson Chinque
  */
-public class SetSlotsController implements ActionListener{
+public class SetSlotsController implements ActionListener {
+
     private final SetSlots view;
+    private JDateChooser jDateChooser;
+    Date date;
+    private ArrayList<String> times;
 
     public SetSlotsController(SetSlots view) {
         this.view = view;
@@ -22,7 +30,38 @@ public class SetSlotsController implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        switch (e.getActionCommand()) {
+            case "add":
+                if (this.view.getAll()) {
+                    
+                    times = new ArrayList<String>();
+                    times.add("1"+"2"+"3"+"4"+"5");
+                    times.add("10:00");
+                    times.add("11:00");
+                    times.add("12:00");
+                    times.add("13:00");
+                    times.add("14:00");
+                    times.add("15:00");
+                    times.add("16:00");
+                    times.add("17:00");
+                    times.add("18:00");
+                    times.add("18:00");
+
+                    date = view.jDateChooser.getDate();
+                    System.out.println(times);
+                    System.out.println(date);
+                    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+                    System.out.println(formato.format(date));
+                    this.view.getConfirmTextArea().setText("Added the day " + formato.format(date));
+                    System.out.println(times.get(0));
+                    break;
+                } else {
+                    if (view.getBox9()){times.add("9:00");}
+                    
+                }
+                break;
+        }
     }
-    
+
 }
