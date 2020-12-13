04e84674dfb604c7b67407804e7a9ba0ba55c829
alychinque <alychinque@gmail.com>
2020-12-13 05:04:51 +0000

    Create Appointment.java

---
+++ src/model/Appointment.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Alysson Chinque
+ */
+public class Appointment {
+    private int id_customer; 
+    private int id_barber; 
+    private String date;
+    private String time;
+    private String status;
+    
+}
