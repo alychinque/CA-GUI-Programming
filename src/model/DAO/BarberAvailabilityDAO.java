75db7ec4997ce8afaee1c17f3b51be88c1512184
alychinque <alychinque@gmail.com>
2020-12-13 23:20:28 +0000

    Two methods were created in BarberAvailability class.

--- src/model/DAO/BarberAvailabilityDAO.java
+++ src/model/DAO/BarberAvailabilityDAO.java
@@ -7,8 +7,10 @@ package model.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
+import javax.swing.JOptionPane;
 import model.BarberAvailability;
 
 /**
@@ -16,6 +18,7 @@ import model.BarberAvailability;
  * @author alych
  */
 public class BarberAvailabilityDAO {
+
     private final Connection connection;
 
     public BarberAvailabilityDAO(Connection connection) {
@@ -23,16 +26,62 @@ public class BarberAvailabilityDAO {
     }
 
     public void insert(BarberAvailability availability) throws SQLException {
-         String query = "INSERT INTO barber_availability (id_barber, date, time)"
+        String query = "INSERT INTO barber_availability (id_barber, date, time)"
                 + "VALUES (?, ?, ?)";
         // Get a statement from the connection
         PreparedStatement stmt = connection.prepareStatement(query);
         stmt.setInt(1, availability.getId());
         stmt.setString(2, availability.getDateAva());
         stmt.setString(3, Arrays.toString(availability.getTime()));
-        
+
         stmt.execute();
     }
+
+    public Boolean checkDay(BarberAvailability availability) throws SQLException {
+        String query = "SELECT * FROM barber_availability WHERE id_barber = ? AND date = ?";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setInt(1, availability.getId());
+        stmt.setString(2, availability.getDateAva());
+        stmt.execute();
+        //catch the result
+        ResultSet resultSet = stmt.getResultSet();
+        
+        //if there are data it shows, otherwise return true and insert
+        if (resultSet.next()) {
+        
+            //it gets the barbers' details from DB and store in new variables
+            int id = resultSet.getInt("id_barber");
+            String date = resultSet.getString("date");
+            String time = resultSet.getString("time");
+           
+            //if barber already save data it asks if they want to overwrite 
+            if (id == availability.getId() && date.equals(availability.getDateAva())) {
+                
+                int returnValue = JOptionPane.showConfirmDialog(null, "You have already set the day " + date
+                        + "\nwith times: " + time
+                        + "\nDo you want overwrite?", "Day already set", JOptionPane.YES_NO_OPTION);
+
+                // if yes it deletes the day, then return true and insert the new times
+                if (returnValue == JOptionPane.YES_OPTION) {
+                    JOptionPane.showMessageDialog(null, "Ok.");
+                    deleteDay(id, date);
+                    return true;
+                    
+                //if no it shows message, returns false and waits the new date 
+                } else if (returnValue == JOptionPane.NO_OPTION) {
+                    JOptionPane.showMessageDialog(null, "Change the day, Please");
+                    return false;
+                }
+            }return true;
+        }return true;
+    }
     
-    
+    public void deleteDay(int id, String date) throws SQLException{
+        String query = "DELETE FROM barber_availability WHERE id_barber = ? AND date = ?";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setInt(1, id);
+        stmt.setString(2, date);
+        stmt.execute();
+    }    
+
 }
