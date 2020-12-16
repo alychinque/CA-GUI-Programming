37ed912566e611fbc2e8ec233c4855b8a3380331
alychinque <alychinque@gmail.com>
2020-12-16 16:49:05 +0000

    ActionListener was created in controller of makeApp.

--- src/controller/makeAppointmentController.java
+++ src/controller/makeAppointmentController.java
@@ -7,6 +7,13 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import model.DAO.BarberAvailabilityDAO;
+import model.DAO.ConnectionDB;
 import view.Customer;
 import view.MakeAppointment;
 
@@ -15,21 +22,41 @@ import view.MakeAppointment;
  * @author Alysson Chinque
  */
 public class makeAppointmentController implements ActionListener {
+
     private final MakeAppointment view;
+    private Connection conn;
 
     public makeAppointmentController(MakeAppointment view) {
         this.view = view;
-        
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("back")) {
-            this.view.setVisible(false);
-            Customer customer = new Customer();
-           // customer.Customer();
+        try {
+            conn = new ConnectionDB().getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(CustomerController.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Failed");
+        }
+        switch (e.getActionCommand()) {
+            case "pickedDay":
+                BarberAvailabilityDAO barberAvailabilityDAO = new BarberAvailabilityDAO(conn);
+
+                try {
+                    this.view.setTimes(barberAvailabilityDAO.search(this.view.getId(), "time"));
+                } catch (SQLException ex) {
+                    Logger.getLogger(makeAppointmentController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                break;
+
+            case "back":
+                this.view.setVisible(false);
+                Customer customer = new Customer();
+                // customer.Customer();
+                break;
         }
     }
-    
-    
+
 }
