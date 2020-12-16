e6cd02c542bb907f900b94ed38de1a28b95703f1
alychinque <alychinque@gmail.com>
2020-12-16 22:35:05 +0000

    Create MakeAppointmentController.java

---
+++ src/controller/MakeAppointmentController.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import model.DAO.BarberAvailabilityDAO;
+import model.DAO.ConnectionDB;
+import view.ChooseTime;
+import view.Customer;
+import view.MakeAppointment;
+
+/**
+ *
+ * @author Alysson Chinque
+ */
+public class MakeAppointmentController implements ActionListener {
+
+    private final MakeAppointment view;
+    private Connection conn;
+
+    public MakeAppointmentController(MakeAppointment view) {
+        this.view = view;
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            conn = new ConnectionDB().getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(CustomerController.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Failed");
+        }
+        switch (e.getActionCommand()) {
+            case "pickedDay":
+                BarberAvailabilityDAO barberAvailabilityDAO = new BarberAvailabilityDAO(conn);
+                ChooseTime chooseTime = new ChooseTime();
+                chooseTime.setDay(this.view.getBoxDay());
+                try {
+                    chooseTime.setTimes(barberAvailabilityDAO.searchTime(this.view.getId(), this.view.getBoxDay()));
+                } catch (SQLException ex) {
+                    Logger.getLogger(MakeAppointmentController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                chooseTime.ChooseTime(this.view.getValidUser());
+                this.view.dispose();
+                
+                break;
+
+            case "back":
+                this.view.dispose();
+                break;
+        }
+    }
+
+}
