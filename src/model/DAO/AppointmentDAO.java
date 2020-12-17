474960203db834ab2ed761ad6c3ff9ab8875afd0
alychinque <alychinque@gmail.com>
2020-12-17 03:17:08 +0000

    Created returnAppointment method.

--- src/model/DAO/AppointmentDAO.java
+++ src/model/DAO/AppointmentDAO.java
@@ -7,8 +7,11 @@ package model.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import model.Appointment;
+import model.ShowAppointment;
 
 /**
  *
@@ -33,5 +36,35 @@ public class AppointmentDAO {
         stmt.setString(5, appointment.getStatus());
 
         stmt.execute();
+    }
+
+    public ArrayList<ShowAppointment> returnAppointmet(int id) throws SQLException {
+        String query = "SELECT barber.name_barber, barber.barbershop, barber.address, barber.location date, time, status \n"
+                + " FROM Alysson_2019305.appointment\n"
+                + " INNER JOIN barber ON barber.id_barber = appointment.id_barber\n"
+                + " where appointment.id_customer = ?;";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setInt(1, id);
+        stmt.execute();
+        //catch the result
+        ResultSet resultSet = stmt.getResultSet();
+
+        ShowAppointment appointment;
+        ArrayList<ShowAppointment> retAppointment = new ArrayList<>();
+        //if there are data it shows, otherwise return true and insert
+        while (resultSet.next()) {
+            //it gets the appointment and barber' details from DB and store in new variables
+            String nameBarber = resultSet.getString("name_barber");
+            String barbershop = resultSet.getString("barbershop");
+            String address = resultSet.getString("address");
+            String location = resultSet.getString("location");
+            String date = resultSet.getString("date");
+            String time = resultSet.getString("time");
+            String status = resultSet.getString("status");
+            
+            appointment = new ShowAppointment(nameBarber, barbershop, address, location, date, time, status);
+            retAppointment.add(appointment);
+        }
+        return retAppointment;
     }
 }
