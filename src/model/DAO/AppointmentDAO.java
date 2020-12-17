c91af446b9fa44888c8b82078ca33f7e6a4c22a8
alychinque <alychinque@gmail.com>
2020-12-17 01:59:04 +0000

    Created insertAppointment method in AppointmentDAO.

--- src/model/DAO/AppointmentDAO.java
+++ src/model/DAO/AppointmentDAO.java
@@ -6,17 +6,32 @@
 package model.DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import model.Appointment;
 
 /**
  *
  * @author Alysson Chinque
  */
 public class AppointmentDAO {
-    
+
     private final Connection connection;
-    
-    public AppointmentDAO(Connection connection){
+
+    public AppointmentDAO(Connection connection) {
         this.connection = connection;
     }
-    
+
+    public void insertAppointment(Appointment appointment) throws SQLException {
+        String query = "INSERT INTO appointment (id_customer, id_barber, date, time, status) VALUES(?, ?, ?, ?, ?);";
+        // Get a statement from the connection
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setInt(1, appointment.getIdCustomer());
+        stmt.setInt(2, appointment.getIdBarber());
+        stmt.setString(3, appointment.getDate());
+        stmt.setString(4, appointment.getTime());
+        stmt.setString(5, appointment.getStatus());
+
+        stmt.execute();
+    }
 }
