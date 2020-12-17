1dfa4064e10f0ec2652d22dd4086489a83e1594e
alychinque <alychinque@gmail.com>
2020-12-17 03:16:32 +0000

    Created ShowAppointment class and added getters and constructor.

---
+++ src/model/ShowAppointment.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Alysson Chinque
+ */
+public class ShowAppointment {
+    
+    private String nameBarber ;
+    private String barberShop ;
+    private String address ;
+    private String location ;
+    private String day ;
+    private String time ;
+    private String status ;
+
+    public ShowAppointment(String nameBarber, String barberShop, String address, String location, String day, String time, String status) {
+        this.nameBarber = nameBarber;
+        this.barberShop = barberShop;
+        this.address = address;
+        this.location = location;
+        this.day = day;
+        this.time = time;
+        this.status = status;
+    }
+
+    public String getNameBarber() {
+        return nameBarber;
+    }
+
+    public String getBarberShop() {
+        return barberShop;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    
+    
+}
