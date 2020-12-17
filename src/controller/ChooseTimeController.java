99a074acfed3198f645eb74540959e9e03741ad3
alychinque <alychinque@gmail.com>
2020-12-17 02:16:14 +0000

    Added connection DB and call method insertAppointment.

--- src/controller/ChooseTimeController.java
+++ src/controller/ChooseTimeController.java
@@ -7,8 +7,14 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import model.Appointment;
+import model.DAO.AppointmentDAO;
+import model.DAO.ConnectionDB;
 import view.ChooseTime;
 import view.Customer;
 
@@ -19,6 +25,7 @@ import view.Customer;
 public class ChooseTimeController implements ActionListener {
 
     private final ChooseTime view;
+    private Connection conn;
 
     public ChooseTimeController(ChooseTime view) {
         this.view = view;
@@ -26,6 +33,12 @@ public class ChooseTimeController implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+         try {
+            conn = new ConnectionDB().getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(CustomerController.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Failed");
+        }
         if (e.getActionCommand().equals("time")) {
             int idUser = this.view.getValidUser().getId();
             int idBarber = this.view.getBarberAvailability().get(0).getId();
@@ -33,7 +46,13 @@ public class ChooseTimeController implements ActionListener {
             String time = this.view.getBoxTime();
 
             Appointment appointment = new Appointment(idUser, idBarber, day, time, "pendent");
-
+            AppointmentDAO appointmentDAO = new AppointmentDAO(conn);
+            try {
+                appointmentDAO.insertAppointment(appointment);
+            } catch (Exception ew) {
+                JOptionPane.showMessageDialog(view, "Failed making the appointment");
+            }
+            
             JOptionPane.showMessageDialog(view, "Congrats you have made an appointment\n"
                     + "Barber: " + this.view.getNameBarber() + "on " + day + "at " + time
                     + "\nThe current status is 'PENDENT' it is waiting for barber approval");
