db6bdc02729a0bf3118cef73e22898c97402c29c
alychinque <alychinque@gmail.com>
2020-12-17 01:58:16 +0000

    Update Appointment.java

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -10,61 +10,42 @@ package model;
  * @author Alysson Chinque
  */
 public class Appointment {
-    private int id_customer; 
-    private int id_barber; 
+    private int idCustomer; 
+    private int idBarber; 
     private String date;
     private String time;
     private String status;
 
     public Appointment(int id_customer, int id_barber, String date, String time, String status) {
-        this.id_customer = id_customer;
-        this.id_barber = id_barber;
+        this.idCustomer = id_customer;
+        this.idBarber = id_barber;
         this.date = date;
         this.time = time;
         this.status = status;
     }
 
-    public int getId_customer() {
-        return id_customer;
+    public int getIdCustomer() {
+        return idCustomer;
     }
 
-    public void setId_customer(int id_customer) {
-        this.id_customer = id_customer;
-    }
-
-    public int getId_barber() {
-        return id_barber;
-    }
-
-    public void setId_barber(int id_barber) {
-        this.id_barber = id_barber;
+    public int getIdBarber() {
+        return idBarber;
     }
 
     public String getDate() {
         return date;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
-
     public String getTime() {
         return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
     }
 
     public String getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
     
     
-    
+   
 }
  
