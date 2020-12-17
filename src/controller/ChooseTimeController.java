635d7be11bdf1f3191c32f5ee7da5f8f389153ce
alychinque <alychinque@gmail.com>
2020-12-17 01:57:54 +0000

    Added a message to show appointment has been made.

--- src/controller/ChooseTimeController.java
+++ src/controller/ChooseTimeController.java
@@ -7,6 +7,7 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 import model.Appointment;
 import view.ChooseTime;
 import view.Customer;
@@ -15,7 +16,8 @@ import view.Customer;
  *
  * @author Alysson Chinque
  */
-public class ChooseTimeController implements ActionListener{
+public class ChooseTimeController implements ActionListener {
+
     private final ChooseTime view;
 
     public ChooseTimeController(ChooseTime view) {
@@ -24,19 +26,23 @@ public class ChooseTimeController implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equals("time")){
+        if (e.getActionCommand().equals("time")) {
             int idUser = this.view.getValidUser().getId();
             int idBarber = this.view.getBarberAvailability().get(0).getId();
             String day = this.view.getDay();
             String time = this.view.getBoxTime();
-            
+
             Appointment appointment = new Appointment(idUser, idBarber, day, time, "pendent");
-            
+
+            JOptionPane.showMessageDialog(view, "Congrats you have made an appointment\n"
+                    + "Barber: " + this.view.getNameBarber() + "on " + day + "at " + time
+                    + "\nThe current status is 'PENDENT' it is waiting for barber approval");
+
             Customer customer = new Customer();
             customer.Customer(this.view.getValidUser());
             this.view.dispose();
         }
-        
+
     }
-    
+
 }
