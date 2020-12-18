41344de73af75473e727430712bc03023dad3111
alychinque <alychinque@gmail.com>
2020-12-18 02:01:22 +0000

    Update LoginController.java

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -25,31 +25,29 @@ import view.Login;
  * @author Alysson Chinque
  */
 public class LoginController implements ActionListener {
+
     private final Login view;
-    
+
     public LoginController(Login view) {
         this.view = view;
-       
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
         switch (e.getActionCommand()) {
             case "combo":
-                if(this.view.getCombo().equals("CUSTOMER")){
+                if (this.view.getCombo().equals("CUSTOMER")) {
                     this.view.changeVisibilityCustomer(true);
                     this.view.changeVisibilityBarber(false);
                     break;
-                }else{
+                } else {
                     this.view.changeVisibilityCustomer(false);
                     this.view.changeVisibilityBarber(true);
-                }   break;
-            case "back":
-                Home home = new Home();
-                this.view.setVisible(false);
-                home.setVisible(true);
+                }
                 break;
+
             case "customer":
                 try {
                     //creating a new Connection conn and giving connection with the DB
@@ -57,23 +55,23 @@ public class LoginController implements ActionListener {
 
                     //instantiating userDAO passing a connection as a parameter
                     UserDAO userDAO = new UserDAO(conn);
-                    
+
                     //data customer
                     String email = this.view.getEmailCustomer().getText();
                     String pass = this.view.getPassCustomer().getText();
-                    
-                    if (email.equals("") || pass.equals("")){
+
+                    if (email.equals("") || pass.equals("")) {
                         JOptionPane.showMessageDialog(null, "Please fill the fields up!");
                         break;
                     }
 
                     //accessing method checkCustumer in the userDAO class passing email and password as a parameter
                     User validUser = userDAO.checkCustomer(email, pass);
-                    
-                    if (validUser == null){
+
+                    if (validUser == null) {
                         JOptionPane.showMessageDialog(null, "User not found!\nPlease enter a valid email and password!\nIf you are new, please back and register");
                         break;
-                    }else{
+                    } else {
                         Customer cust = new Customer();
                         cust.Customer(validUser);
                         this.view.setVisible(false);
@@ -82,7 +80,7 @@ public class LoginController implements ActionListener {
                     JOptionPane.showMessageDialog(null, "Error");
                     break;
                 }
-                
+
                 break;
             case "barber":
                 try {
@@ -91,23 +89,23 @@ public class LoginController implements ActionListener {
 
                     //instantiating userDAO passing a connection as a parameter
                     BarberDAO barberDAO = new BarberDAO(conn);
-                    
+
                     //data customer
                     String email = this.view.getEmailBarber().getText();
                     String pass = this.view.getPassBarber().getText();
-                    
-                    if (email.equals("") || pass.equals("")){
+
+                    if (email.equals("") || pass.equals("")) {
                         JOptionPane.showMessageDialog(null, "Please fill the fields up!");
                         break;
                     }
 
                     //accessing method checkCustumer in the userDAO class passing email and password as a parameter
                     Barber validBarber = barberDAO.checkBarber(email, pass);
-                    
-                    if (validBarber == null){
+
+                    if (validBarber == null) {
                         JOptionPane.showMessageDialog(null, "Barber not found!\nPlease enter a valid email and password!\nIf you are new, please back and register");
                         break;
-                    }else{
+                    } else {
                         BarberView bar = new BarberView();
                         bar.BarberView(validBarber);
                         this.view.setVisible(false);
@@ -116,9 +114,14 @@ public class LoginController implements ActionListener {
                     JOptionPane.showMessageDialog(null, "Error");
                     break;
                 }
-                
+
+                break;
+            case "back":
+                Home home = new Home();
+                this.view.setVisible(false);
+                home.setVisible(true);
                 break;
         }
     }
-    
+
 }
