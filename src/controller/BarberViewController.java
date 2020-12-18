7c630df82ae8dcaeab1999e887ed0bd6bfca6a09
alychinque <alychinque@gmail.com>
2020-12-18 08:28:11 +0000

    Added ActionListener for show appointments in BarberViewController..

--- src/controller/BarberViewController.java
+++ src/controller/BarberViewController.java
@@ -7,6 +7,14 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import model.DAO.AppointmentDAO;
+import model.DAO.ConnectionDB;
+import view.BarberAppointments;
 import view.BarberView;
 import view.SetSlots;
 
@@ -14,27 +22,51 @@ import view.SetSlots;
  *
  * @author Alysson Chinque
  */
-public class BarberViewController implements ActionListener{
+public class BarberViewController implements ActionListener {
+
     private final BarberView view;
-    
+    private Connection conn;
+
     public BarberViewController(BarberView view) {
         this.view = view;
     }
-    
-    
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        //creating a new Connection conn and giving connection with the DB
+        try {
+            conn = new ConnectionDB().getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(CustomerController.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Failed");
+        }
+
         switch (e.getActionCommand()) {
+            case "show":
+                AppointmentDAO appointmentDAO = new AppointmentDAO(conn);
+                String[][] barberAppointment = null;
+                 {
+                    try {
+                        barberAppointment = appointmentDAO.barberApointment(this.view.getValidBarber().getId());
+                        if (barberAppointment == null) {
+
+                        }
+                    } catch (SQLException ex) {
+                        Logger.getLogger(BarberViewController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                     BarberAppointments barberAppointments = new BarberAppointments();
+                     barberAppointments.setData(barberAppointment);
+                     barberAppointments.BarberAppointments(this.view);
+                     this.view.dispose();
+                }
+
             case "setSlots":
                 SetSlots setSlots = new SetSlots();
                 setSlots.SetSlots(this.view.getValidBarber());
                 this.view.dispose();
                 break;
-                
-                
+
         }
     }
-    
+
 }
