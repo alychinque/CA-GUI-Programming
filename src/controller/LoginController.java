d5d5b48d23f674773874e8f75504a3863f8af798
alychinque <alychinque@gmail.com>
2020-12-18 06:41:17 +0000

    Update LoginController.java

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -9,8 +9,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import model.Barber;
+import model.DAO.AppointmentDAO;
 import model.DAO.ConnectionDB;
 import model.DAO.UserDAO;
 import model.DAO.BarberDAO;
@@ -27,6 +30,7 @@ import view.Login;
 public class LoginController implements ActionListener {
 
     private final Login view;
+    private Connection conn;
 
     public LoginController(Login view) {
         this.view = view;
@@ -35,6 +39,13 @@ public class LoginController implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        //creating a new Connection conn and giving connection with the DB
+
+        try {
+            conn = new ConnectionDB().getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         switch (e.getActionCommand()) {
             case "combo":
@@ -50,8 +61,6 @@ public class LoginController implements ActionListener {
 
             case "customer":
                 try {
-                    //creating a new Connection conn and giving connection with the DB
-                    Connection conn = new ConnectionDB().getConnection();
 
                     //instantiating userDAO passing a connection as a parameter
                     UserDAO userDAO = new UserDAO(conn);
@@ -84,8 +93,6 @@ public class LoginController implements ActionListener {
                 break;
             case "barber":
                 try {
-                    //creating a new Connection conn and giving connection with the DB
-                    Connection conn = new ConnectionDB().getConnection();
 
                     //instantiating userDAO passing a connection as a parameter
                     BarberDAO barberDAO = new BarberDAO(conn);
@@ -106,9 +113,9 @@ public class LoginController implements ActionListener {
                         JOptionPane.showMessageDialog(null, "Barber not found!\nPlease enter a valid email and password!\nIf you are new, please back and register");
                         break;
                     } else {
-                        BarberView bar = new BarberView();
-                        bar.BarberView(validBarber);
-                        this.view.setVisible(false);
+                        BarberView barView = new BarberView();
+                        barView.BarberView(validBarber);
+                        this.view.dispose();
                     }
                 } catch (SQLException ex) {
                     JOptionPane.showMessageDialog(null, "Error");
